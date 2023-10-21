7b8500a1daa7d65724c07aac1d9bb7c40d40e5d6
blurryfacebeat <blurryfacebeat@gmail.com>
2023-10-21 15:05:13 +0700

    feat: Remove mongo and add TypeORM and pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { getMongoConfig } from './configs/mongo.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getMongoConfig,
-    }),
     AuthModule,
     TopPageModule,
     ProductModule,
